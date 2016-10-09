288e52879b1d470d6c5386f86157eada82b2600d
vishaknag <vishaknag@gmail.com>
2016-10-09 15:11:45 -0700

    to Vanilla JS

--- preview/edmodo/edmodo.js
+++ preview/edmodo/edmodo.js
@@ -1,96 +1,125 @@
-var clicked = false;
 function initFloatingAd() {
     $(document.body).append($('#floating-items').detach());
-    $('.str-adunit').click(function(e) {
-        console.log('user clicked ad');
-        clicked = true;
+    var adUnit = document.getElementsByClassName('str-adunit')[0];
+    console.log(adUnit);
+    adUnit.onclick = function(e) {
+        e.stopPropagation();
+        userClicked = true;
 
         // Only for Content and Leadgen Ads
-        if($('#floating-ad-container').length) {
-            var adUnitOffset = $('.str-adunit').offset(),
-                floatUnitOffset = $('#floating-ad-container').offset();
-            // Start moving the floating container to overlap infeed unit
-            $('#floating-ad-container').css({ "top": adUnitOffset.top, "left": adUnitOffset.left });
-
-            if(adUnitOffset.top === floatUnitOffset.top) {
-                showFloatingContainer();
-            } else {
-                $('#floating-ad-container').on("webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend", function(event) {
-                    showFloatingContainer();
-                });
-            }
-
-            return false;
+        var floatingUnit = document.getElementById('floating-ad-container');
+        if(hasClass(floatingUnit, 'animate')) {
+            console.log('Remove animation from floating unit');
+            removeClass(floatingUnit, 'animate');
         }
-    });
+        overlapFloatingContainer(document.getElementsByClassName('str-adunit')[0]);
+        showFloatingContainer();
+    }
 
-    $('.str-ico-close, .floating-bg').click(function(e) {
-        e.stopPropagation();
-        e.preventDefault();
-        console.log('user closed ad');
-        var offset = $('.str-adunit').offset();
-        $('#floating-ad-container, .floating-bg').removeClass('clicked');
-        $('#floating-ad-container').css({ "top": offset.top, "left": offset.left });
-    });
+    if(document.getElementsByClassName('str-ico-close').length) {
+        document.getElementsByClassName('str-ico-close')[0].onclick = adClosed;
+    }
+    if(document.getElementsByClassName('floating-bg').length) {
+        document.getElementsByClassName('floating-bg')[0].onclick = adClosed;
+    }
 }
 
 function showFloatingContainer() {
-    if(!clicked) return;
-    clicked = false;
+    if(!userClicked) return;
+    userClicked = false;
 
-    var floatContainer = $('#floating-ad-container');
-    console.log('moved to adunit pos!');
-    if(floatContainer.hasClass('install')) return;
+    var floatingUnit = document.getElementById('floating-ad-container');
+    if(hasClass(floatingUnit, 'install')) return;
+
+    // Add animation to floating container
+    if(!hasClass(floatingUnit, 'animate')) {
+        console.log('Add animation to floating unit');
+        addClass(floatingUnit, 'animate');
+    }
 
     // Increase height for Content Ad
-    if(floatContainer.hasClass('content')) floatContainer.css('height', $('.str-adunit').height() + 'px');
+    if(hasClass(floatingUnit, 'content')) floatingUnit.style.height = $('.str-adunit').height() + 'px';
+    if(hasClass(floatingUnit, 'clicked')) return;
 
-    if(floatContainer.hasClass('clicked')) return;
-    floatContainer.addClass('clicked');
+    // Animate floating container and add listener
+    var transtionEvent = whichTransitionEvent();
+    transtionEvent && floatingUnit.addEventListener(transtionEvent, function() {
+        if(!hasClass(floatingUnit.querySelector('#content-container'), 'clicked')) {
+            console.log('Float ad container animation complete. Show iframe');
+            addClass(floatingUnit.querySelector('#content-container'), 'clicked');
+        }
+    });
 
-    var adContainerOffset = getViewportOffset($("#floating-ad-container"));
-    $('#floating-ad-container, .floating-bg').addClass('clicked');
+    addClass(floatingUnit, 'clicked');
+    addClass(document.getElementsByClassName('floating-bg')[0], 'clicked');
 
-    // Onclick move FC to full height of viewport
-    if($('.str-adunit.content-ad').length) {
-        floatContainer.css("top", "0px");
+    if(document.querySelector('.str-adunit.content-ad')) {
+        floatingUnit.style.top = "0px";
     } else {
-        floatContainer.css("top", "150px");
+        floatingUnit.style.top = "150px";
     }
 
     // Start Video Play
     iframe = document.getElementsByClassName('adsnative-video-iframe')[0];
-    if(iframe) iframe.contentWindow.postMessage('adsnative.mrc50.view:in', 'http://api.adsnative.com'); 
+    if(iframe) iframe.contentWindow.postMessage('adsnative.mrc50.view:in', 'http://api.adsnative.com');
 }
 
-function whichAnimationEvent() {
-    var t,
-        el = document.createElement("fakeelement");
+function adClosed(e) {
+    e.stopPropagation();
+    var floatingUnit = document.getElementById('floating-ad-container');
+    if(hasClass(floatingUnit, 'animate')) console.log('Has animate class');
+    overlapFloatingContainer(document.getElementsByClassName('str-adunit')[0]);
 
-    var animations = {
-        "animation": "animationend",
-        "OAnimation": "oAnimationEnd",
-        "MozAnimation": "animationend",
-        "WebkitAnimation": "webkitAnimationEnd"
+    removeClass(floatingUnit, 'clicked');
+    removeClass(document.querySelector('.floating-bg'), 'clicked');
+    removeClass(floatingUnit.querySelector('#content-container'), 'clicked');
+}
+
+function overlapFloatingContainer(adUnit) {
+    var floatingUnit = document.getElementById('floating-ad-container');
+    if(!floatingUnit) return;
+
+    floatingUnit.style.top = adUnit.getBoundingClientRect().top;
+    floatingUnit.style.left = adUnit.getBoundingClientRect().left;
+
+    console.log('Overlapped at Top : ', floatingUnit.style.top, 'Left : ', floatingUnit.style.left);
+}
+
+function hasClass(el, className) {
+  if (el.classList)
+    return el.classList.contains(className)
+  else
+    return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'))
+}
+
+function addClass(el, className) {
+  if (el.classList)
+    el.classList.add(className)
+  else if (!hasClass(el, className)) el.className += " " + className
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className)
+  else if (hasClass(el, className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)')
+    el.className=el.className.replace(reg, ' ')
+  }
+}
+
+function whichTransitionEvent(){
+    var t;
+    var el = document.createElement('fakeelement');
+    var transitions = {
+      'transition':'transitionend',
+      'OTransition':'oTransitionEnd',
+      'MozTransition':'transitionend',
+      'WebkitTransition':'webkitTransitionEnd'
     }
 
-    for (t in animations) {
-        if (el.style[t] !== undefined) {
-            return animations[t];
+    for(t in transitions){
+        if( el.style[t] !== undefined ){
+            return transitions[t];
         }
     }
-}
-
-function getViewportOffset($e) {
-    var $window = $(window),
-        scrollLeft = $window.scrollLeft(),
-        scrollTop = $window.scrollTop(),
-        offset = $e.offset(),
-        rect1 = { x1: scrollLeft, y1: scrollTop, x2: scrollLeft + $window.width(), y2: scrollTop + $window.height() },
-        rect2 = { x1: offset.left, y1: offset.top, x2: offset.left + $e.width(), y2: offset.top + $e.height() };
-    return {
-        left: offset.left - scrollLeft,
-        top: offset.top - scrollTop,
-        insideViewport: rect1.x1 < rect2.x2 && rect1.x2 > rect2.x1 && rect1.y1 < rect2.y2 && rect1.y2 > rect2.y1
-    };
 }
