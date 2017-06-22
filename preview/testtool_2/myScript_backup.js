379abf879215b054717b6547edcd5b8ea280d30d
Shaheeda <shaheeda@adsnative.com>
2017-06-21 17:23:48 -0700

    testool dg

--- preview/testtool_2/myScript_backup.js
+++ preview/testtool_2/myScript_backup.js
@@ -222,9 +222,6 @@ var iframe_mapping = {
         ],
 
         "advancenative": [
-            "http://docs.adsnative.com/preview/advancenative_ads.html?adsnative_preview=1&cid=11994",
-            "http://docs.adsnative.com/preview/advancenative_ads.html?adsnative_preview=1&cid=11994",
-            "http://docs.adsnative.com/preview/advancenative-rr.html?adsnative_preview=1&cid=11994"
         ],
 
         "viber": [
